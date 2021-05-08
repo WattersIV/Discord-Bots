b3096ebfb0819ea73af47882948b7dd968efe0bb
Bill Watters <billywatters13@gmail.com>
2021-05-08 19:06:02 -0400

    Confucius command pulls random quotes

--- meme-bot/src/commands/confucius/ConfuciusCommand.js
+++ meme-bot/src/commands/confucius/ConfuciusCommand.js
@@ -1,3 +1,5 @@
+const axios = require('axios');
+const { parse } =  require('node-html-parser');
 const BaseCommand = require('../../utils/structures/BaseCommand');
 
 module.exports = class ConfuciusCommand extends BaseCommand {
@@ -6,6 +8,18 @@ module.exports = class ConfuciusCommand extends BaseCommand {
   }
 
   run(client, message, args) {
-    message.channel.send('Confucius say, man under wheelbarrow playing with tool, not necessarily mechanic');
+    axios.get('http://www.trees-and-lambdas.info/matushansky/confucius.html')
+      .then((res) => {
+        const root = parse(`${res.data}`)
+        //Selecting all of the li elements which are all of the quotes on this site
+        const quotes = root.querySelectorAll('li')
+        const quoteNumber = this.randomInt(quotes.length - 1)
+        message.channel.send(`${quotes[quoteNumber].rawText}`);
+      })
+  }
+
+  randomInt(max) {
+    //Gen random number between 0 and the max number given
+    return Math.floor(Math.random() * (max + 1));
   }
 }
